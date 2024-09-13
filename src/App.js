10550f55c6cf525fd83ab050f85ca4674a2484cf
Jamie Weldon <jamie.weldon@outlook.com>
2024-09-13 10:37:35 -0500

    apply select styles

--- src/App.js
+++ src/App.js
@@ -32,38 +32,35 @@ function App() {
     <div className="container p-3">
       <h1>Product Listing Page</h1>
       <div className="container">
-        <div className="row p-2 justify-content-end">
-          <div className="col-lg-3 p-1 ml-auto">
-            <label>
-              Filter by color:
-              <select className="ms-2" onChange={handleFilterChange}>
-                <option value="">All</option>
-                <option value="red">Red</option>
-                <option value="blue">Blue</option>
-                <option value="green">Green</option>
-              </select>
-            </label>
+        <div className="row p-2">
+          <div className="d-flex col-lg-3 p-1">
+            <label className="col-form-label text-nowrap">Filter by color:</label>
+            <select className="form-select form-select-sm filter-select ms-2" onChange={handleFilterChange}>
+              <option value="">All</option>
+              <option value="red">Red</option>
+              <option value="blue">Blue</option>
+              <option value="green">Green</option>
+            </select>
           </div>
-          <div className="col-lg-3 p-1">
-            <label>
-              Sort by:
-              <select className="ms-2" onChange={handleSortChange}>
-                <option value="">None</option>
-                <option value="price">Price</option>
-                <option value="name">Name</option>
-              </select>
-            </label>
+          <div className="d-flex col-lg-3 p-1">
+            <label className="col-form-label text-nowrap">Sort by:</label>
+            <select className="form-select form-select-sm filter-select ms-2" onChange={handleSortChange}>
+              <option value="">None</option>
+              <option value="price">Price</option>
+              <option value="name">Name</option>
+            </select>
           </div>
         </div>
       </div>
       <div className="container product-grid">
         {sortedProducts.map((product) => (
           <div key={product.id} className="card">
-            <img src={product.image} alt={product.name} className="rounded"/>
-            <div class="card-body">
-              <h5 class="card-title">{product.name}</h5>
-              <h6 class="card-subtitle">${product.price.toFixed(2)}</h6>
-              <p class="card-text">Colors: {product.colors.join(", ")}</p>
+            <img src={product.image} alt={product.name} className="card-img-top"/>
+            <div className="card-body">
+              <h5 className="card-title">{product.name}</h5>
+              <h6 className="card-subtitle">${product.price.toFixed(2)}</h6>
+              <p className="card-text">Colors: {product.colors.join(", ")}</p>
+              <button className="btn btn-primary">Go Somewhere</button>
             </div>
           </div>
         ))}
